56cc38872c6e391f5c3d1ec5518ac20b671fc132
Jamal Frank <jamaljfrank@gmail.com>
2020-03-26 18:52:35 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,6 +4,7 @@ describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
+  end
     
     
   it 'renders HTML in the /new template' do
